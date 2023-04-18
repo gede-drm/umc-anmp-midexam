01de2b1117ee2271b9d2e68b753f042551574f28
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-04-18 23:22:17 +0700

    View Initialisation & Added Gradle Dependencies

--- app/src/main/java/com/geded/ubayamedicalcenter/view/MainActivity.kt
+++ app/src/main/java/com/geded/ubayamedicalcenter/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.geded.ubayamedicalcenter
+package com.geded.ubayamedicalcenter.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geded.ubayamedicalcenter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

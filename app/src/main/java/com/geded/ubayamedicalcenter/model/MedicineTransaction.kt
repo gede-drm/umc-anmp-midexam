6932b7bde7e433dd2dee4fa7aeaf6c6ec88dc2a2
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-04-21 23:11:38 +0700

    Added Volley, GSON, and Picasso Dependencies and Fixing SerializedName error on MedicineTransaction.kt

--- app/src/main/java/com/geded/ubayamedicalcenter/model/MedicineTransaction.kt
+++ app/src/main/java/com/geded/ubayamedicalcenter/model/MedicineTransaction.kt
@@ -1,8 +1,10 @@
 package com.geded.ubayamedicalcenter.model
 
+import com.google.gson.annotations.SerializedName
+
 data class MedicineTransaction(
     val id:String,
     val date:String,
-    @SerializedName('total_price')
+    @SerializedName("total_price")
     val totalPrice: Int
 )

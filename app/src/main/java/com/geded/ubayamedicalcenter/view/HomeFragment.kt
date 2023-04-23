04b7631472076e432bab19e29d87ef7a10f2c979
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-04-23 17:41:13 +0700

    HomeFragment: Added Intent on Emergency Call Button

--- app/src/main/java/com/geded/ubayamedicalcenter/view/HomeFragment.kt
+++ app/src/main/java/com/geded/ubayamedicalcenter/view/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.geded.ubayamedicalcenter.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.geded.ubayamedicalcenter.R
 
 class HomeFragment : Fragment() {
@@ -18,5 +21,19 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnEmergencyCall = view.findViewById<Button>(R.id.btnEmergencyCall)
+
+        btnEmergencyCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:623199852249")
+            if (this.activity?.let { it1 -> intent.resolveActivity(it1.packageManager) } != null) {
+                startActivity(intent)
+            }
+        }
     }
 }
